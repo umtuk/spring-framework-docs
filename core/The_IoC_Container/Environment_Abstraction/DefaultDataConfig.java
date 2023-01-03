7f3e7cf6c7918ec9bd8f18114f049709e29d6f5e
Taeuk Um <42173347+umtuk@users.noreply.github.com>
2023-01-03 17:31:22 +0900

    #1 - 3.1.13.1. Bean Definition Profiles

---
+++ core/The_IoC_Container/Environment_Abstraction/DefaultDataConfig.java
@@ -0,0 +1,22 @@
+package The_IoC_Container.Environment_Abstraction;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
+
+@Configuration
+@Profile("default")
+public class DefaultDataConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.HSQL)
+                .addScript("classpath:com/bank/config/sql/schema.sql")
+                .build();
+    }
+}
